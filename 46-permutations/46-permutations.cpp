00ef3f3ad846079186ec3f9e9c19543d2d372a25
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-02-20 15:42:40 +0530

    Time: 9 ms (19.87%), Space: 7.7 MB (79.30%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,27 +1,40 @@
-class Solution {
+class Solution {//commented part is using extra space
 public:
-    void permutation(vector<int> nums, vector<int> &ds, vector<vector<int>>                               &ans,bool vis[]){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    // void permutation(vector<int> nums, vector<int> &ds, vector<vector<int>>                               &ans,bool vis[]){     //with using extra space
+    //     if(ds.size()==nums.size()){
+    //         ans.push_back(ds);
+    //         return;
+    //     }
+    //     for(int i=0;i<nums.size();i++){
+    //         if(!vis[i]){
+    //             ds.push_back(nums[i]);
+    //             vis[i]=true;
+    //             permutation(nums,ds,ans,vis);
+    //             ds.pop_back();
+    //             vis[i]=false;
+    //         }
+    //     }
+    // }
+    void permutations(vector<int> &nums, vector<vector<int>> &ans, int index){
+        if(index==nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(!vis[i]){
-                ds.push_back(nums[i]);
-                vis[i]=true;
-                permutation(nums,ds,ans,vis);
-                ds.pop_back();
-                vis[i]=false;
-            }
-        }
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            permutations(nums,ans,index+1);
+            swap(nums[index],nums[i]);
+            
+        }        
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
-        bool vis[nums.size()];
-        memset(vis,false,sizeof(vis));
+        // bool vis[nums.size()];
+        // memset(vis,false,sizeof(vis));
         vector<vector<int>> ans;
         vector<int> ds;
-        permutation(nums,ds,ans,vis);
+        // permutation(nums,ds,ans,vis);
+        permutations(nums,ans,0);
         return ans;
     }
 };
