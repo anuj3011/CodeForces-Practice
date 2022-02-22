8ea2f34842c20965fb1c313c947f541f3679ffbf
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-02-22 12:40:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (16.56%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -4,19 +4,19 @@ public:
         int fact=1;
         vector<int> nums;
         for(int i=1;i<n;i++){
-            fact*=i;
-            nums.push_back(i);
+            fact*=i;        //calculating factorial
+            nums.push_back(i);      //storing all the integers
         }
         nums.push_back(n);
         string s="";
-        k--;
+        k--;            //as we are using 0 based indexing reducing k
         while(true){
-            s+=to_string(nums[k/fact]);
-            nums.erase(nums.begin()+k/fact);
+            s+=to_string(nums[k/fact]);             // curr digit will be k/fact, dry run
+            nums.erase(nums.begin()+k/fact);        //remove that digit from vector
             if(nums.size()==0)
                 break;
-            k=k%fact;
-            fact/=nums.size();
+            k=k%fact;                               //reduce k to find next digit
+            fact/=nums.size();                      //reduce factorial to next lower                                                             //intergers fact, i.e. n! to n-1!
         }
         return s;
     }
