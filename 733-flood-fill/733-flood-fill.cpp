9205908477d35c467e49b947200b8f62010475d6
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-02-24 13:25:14 +0530

    Time: 4 ms (98.00%), Space: 14.2 MB (37.81%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int n=image.size();
+        int m=image[0].size();
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++)
+                vis[i][j]=false;
+        }
+        dfs(image,n,m,sr,sc,newColor,vis);
+        image[sr][sc]=newColor;
+        return image;
+    }
+    
+    void dfs(vector<vector<int>> &image, int n, int m, int sr,int sc,int                                     newColor,vector<vector<bool>> &vis){
+        vis[sr][sc]=true;
+        for(int i=-1;i<2;i++){
+            if(validI(sr+i,n)){
+                if(!vis[sr+i][sc]){
+                    if(image[sr+i][sc]==image[sr][sc]){
+                        dfs(image,n,m,sr+i,sc,newColor,vis);
+                        image[sr+i][sc]=newColor;
+                    }
+                }
+            }
+        }
+        for(int i=-1;i<2;i++){
+            if(validJ(sc+i,m)){
+                if(!vis[sr][sc+i]){
+                    if(image[sr][sc+i]==image[sr][sc]){
+                        dfs(image,n,m,sr,sc+i,newColor,vis);
+                        image[sr][sc+i]=newColor;
+                    }
+                }
+            }
+        }
+    }
+    bool validI(int x,int n){
+        if(x<n && x>=0)
+            return true;
+        return false;
+    }
+    bool validJ(int y, int m){
+        if(y<m && y>=0){
+            return true;
+        }
+        return false;
+    }
+};
