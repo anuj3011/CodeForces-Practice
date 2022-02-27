c39e1e62359bfa62820df51158ae3fa440843dc6
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-02-27 12:52:45 +0530

    Time: 646 ms (16.07%), Space: 73.5 MB (30.34%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -4,7 +4,7 @@ public:
         priority_queue<pair<double,vector<int>>> maxh;
         for(int i=0;i<points.size();i++){
             double dist= points[i][0]*points[i][0] + points[i][1]*points[i][1];
-            dist= sqrt(dist);
+            // dist= sqrt(dist);
             maxh.push({dist,points[i]});
             if(maxh.size()>k)
                 maxh.pop();
