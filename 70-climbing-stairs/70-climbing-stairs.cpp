09fc623289bccad3823b847e5bfa0b537afa2c84
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-03-01 15:38:54 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (29.13%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int countWays(int n,vector<int> &dp){
+    int countWays(int n,vector<int> &dp){ //Memoization
         if(n<=1){
             return 1;
         }
@@ -9,10 +9,21 @@ public:
         dp[n-2]=countWays(n-2,dp);
         return dp[n]=dp[n-1]+dp[n-2];
     }
+    int countWays_tabulation(int n,vector<int> &dp){
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return countWays(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return countWays(n,dp); //memoization
         // return dp[n];
+        
+        vector<int> dp(n+1);
+        return countWays_tabulation(n,dp);
     }
 };
