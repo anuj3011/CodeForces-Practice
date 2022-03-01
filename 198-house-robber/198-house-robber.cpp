15d162cdcb44068e593bb9352b374baf7fe6e54d
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-03-01 21:35:00 +0530

    Time: 4 ms (35.86%), Space: 7.7 MB (47.16%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -10,22 +10,37 @@ public:
     //     int notPick=0+f(nums,ind-1,dp);
     //     return dp[ind]=max(pick,notPick);
     // }
-    int rob(vector<int>& nums) {
-        int ind=nums.size()-1;
-        int n=nums.size();
-        // vector<int> dp(ind+1,-1);
-        // f(nums,ind,dp);
-        // return dp[ind];
+//     int rob(vector<int>& nums) { //tabulation
+//         int ind=nums.size()-1;
+//         int n=nums.size();
+//         // vector<int> dp(ind+1,-1);
+//         // f(nums,ind,dp);
+//         // return dp[ind];
         
-        vector<int> dp(n,-1);
-        dp[0]=nums[0];
-        // dp[1]=nums[1];
+//         vector<int> dp(n,-1);
+//         dp[0]=nums[0];
+//         // dp[1]=nums[1];
+//         for(int i=1;i<n;i++){
+//             int pick = nums[i];
+//             if(i>1) pick+= dp[i-2];
+//             int notPick= dp[i-1];
+//             dp[i]=max(pick,notPick);
+//         }
+//         return dp[n-1];
+//     }
+    int rob(vector<int> &nums){
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
         for(int i=1;i<n;i++){
-            int pick = nums[i];
-            if(i>1) pick+= dp[i-2];
-            int notPick= dp[i-1];
-            dp[i]=max(pick,notPick);
+            int pick=nums[i];
+            if(i>1) pick+=prev2;
+            int notPick=prev;
+            int curr=max(pick,notPick);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
     }
+    
 };
