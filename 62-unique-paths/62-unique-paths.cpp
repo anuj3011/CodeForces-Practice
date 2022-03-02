8a7dcd4104d5921228fb1f01707aab873baa705c
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-03-02 17:25:19 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (24.70%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -11,8 +11,21 @@ public:
         return dp[m][n] =prevRow + prevCol;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return countWays(m-1,n-1,dp);
-        
+        // vector<vector<int>> dp(m,vector<int> (n,-1));
+        // return countWays(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int> (n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int left=0,top=0;
+                    if(i>0) left=dp[i-1][j];
+                    if(j>0) top=dp[i][j-1];
+                    dp[i][j]=left+top;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
