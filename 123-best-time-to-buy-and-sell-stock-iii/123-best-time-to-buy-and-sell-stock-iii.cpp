0215595e951473250d1fd0ec75be688788eadfb5
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-03-31 11:53:12 +0530

    Time: 340 ms (21.76%), Space: 132.7 MB (17.29%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -17,30 +17,49 @@ public:
     //     return dp[ind][buy][cap]=profit;
     // }
     
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        // vector<vector<vector<int>>> dp(n, vector<int>(2, vector<int>(3,-1)));
-        // memset(dp,-1,sizeof(dp));
-        // return f(0,1,2,prices); //0 is index, 1 is start from buy, 2 is at max 2 transactions;
+//     int maxProfit(vector<int>& prices) { //tabulation memory optimised
+//         int n=prices.size();
+//         // vector<vector<vector<int>>> dp(n, vector<int>(2, vector<int>(3,-1)));
+//         // memset(dp,-1,sizeof(dp));
+//         // return f(0,1,2,prices); //0 is index, 1 is start from buy, 2 is at max 2 transactions;
         
-        // memset(dp,0,sizeof(dp));
-        vector<vector<int>> after(2,vector<int>(3,0));
-        vector<vector<int>> curr(2,vector<int>(3,0));
+//         // memset(dp,0,sizeof(dp));
+//         vector<vector<int>> after(2,vector<int>(3,0));
+//         vector<vector<int>> curr(2,vector<int>(3,0));
         
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                for(int cap=1;cap<3;cap++){
-                    if(buy){
-                        curr[buy][cap]=max(-prices[i] + after[0][cap],
-                                   after[1][cap]);
-                    }else{
-                        curr[buy][cap]=max(prices[i] + after[1][cap-1],
-                                   after[0][cap]);
-                    }
-                    after=curr;
-                }
-            }
+//         for(int i=n-1;i>=0;i--){
+//             for(int buy=0;buy<=1;buy++){
+//                 for(int cap=1;cap<3;cap++){
+//                     if(buy){
+//                         curr[buy][cap]=max(-prices[i] + after[0][cap],
+//                                    after[1][cap]);
+//                     }else{
+//                         curr[buy][cap]=max(prices[i] + after[1][cap-1],
+//                                    after[0][cap]);
+//                     }
+//                     after=curr;
+//                 }
+//             }
+//         }
+//         return after[1][2];
+//     }
+    
+    int f_n4(int ind, int trans,vector<int> &prices, vector<vector<int>> &dp){
+        if(ind==prices.size() || trans==4)
+            return 0;
+        if(dp[ind][trans]!=-1) return dp[ind][trans];
+        if(trans%2==0){
+            return dp[ind][trans]=max(-prices[ind] + f_n4(ind+1,trans+1,prices,dp),
+                       f_n4(ind+1,trans,prices,dp));
+        }else{
+            return dp[ind][trans]=max(prices[ind] + f_n4(ind+1,trans+1,prices,dp),
+                       f_n4(ind+1,trans,prices,dp));
         }
-        return after[1][2];
     }
+    
+    int maxProfit(vector<int>& prices){ //n*4 memoization
+        vector<vector<int>> dp(prices.size(), vector<int>(4,-1));
+        return f_n4(0,0,prices,dp); //ind and transaction as 0
+    }
+    
 };
