c21df2224ec228216dff6b2d3bc425c8f322cadf
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-03-31 11:59:14 +0530

    Time: 529 ms (15.41%), Space: 135.4 MB (15.19%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -58,8 +58,20 @@ public:
     }
     
     int maxProfit(vector<int>& prices){ //n*4 memoization
-        vector<vector<int>> dp(prices.size(), vector<int>(4,-1));
-        return f_n4(0,0,prices,dp); //ind and transaction as 0
+        // vector<vector<int>> dp(prices.size(), vector<int>(4,-1));
+        // return f_n4(0,0,prices,dp); //ind and transaction as 0
+        
+        vector<vector<int>> dp(prices.size()+1, vector<int>(5,0));
+        for(int ind=prices.size()-1;ind>=0;ind--){
+            for(int trans=3;trans>=0;trans--){
+                if(trans%2==0){
+                    dp[ind][trans]=max(-prices[ind] + dp[ind+1][trans+1],dp[ind+1][trans]);
+                }else{
+                    dp[ind][trans]=max(prices[ind] + dp[ind+1][trans+1],dp[ind+1][trans]);
+                }
+            }
+        }
+      return dp[0][0];  
     }
     
 };
