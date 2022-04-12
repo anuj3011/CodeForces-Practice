240cb1128cba1d4577b4e8983aca42cc272b0a92
anuj3011 <57455202+anuj3011@users.noreply.github.com>
2022-04-12 13:05:47 +0530

    Time: 7 ms (84.24%), Space: 10.5 MB (59.97%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,40 +1,57 @@
 class Solution {
 public:
-   int minSubArrayLen(int k, vector<int>& nums) {
-           int size=INT_MAX;
-        int i=0,j=0;
-        int sum=0;
-        while(j<nums.size()){
-            sum+=nums[j];
-            if(sum<k){
-                j++;
-            }
-            // else if(sum==k)
-            // {
-            //     cout<<"if"<<i<<" "<<j<<endl;
-            //     size=min(j-i+1,size);
-            //     j++;
-            // }
-            else{
-                //  size=min(j-i+1,size);
-                // cout<<"else"<<i<<" "<<j<<endl;
-                while(sum>=k){
-                   size=min(j-i+1,size);
-                    if(nums[i]>0)
-                        sum-=nums[i];
-                    else
-                        sum+=nums[i];
-                    i++;
-                }
-                cout<<"else"<<i<<" "<<j<<endl;
-                // if(i==j)
-                //     size=1;
-                // else
-                // cout<<j-i+1;
-                //     size=min(j-i+2,size);
-                j++;
-            }
+   // int minSubArrayLen(int k, vector<int>& nums) {  //using sliding window
+   //         int size=INT_MAX;
+   //      int i=0,j=0;
+   //      int sum=0;
+   //      while(j<nums.size()){
+   //          sum+=nums[j];
+   //          if(sum<k){
+   //              j++;
+   //          }
+   //          else{
+   //              while(sum>=k){
+   //                 size=min(j-i+1,size);
+   //                  if(nums[i]>0)
+   //                      sum-=nums[i];
+   //                  else
+   //                      sum+=nums[i];
+   //                  i++;
+   //              }
+   //              cout<<"else"<<i<<" "<<j<<endl;
+   //              j++;
+   //          }
+   //      }
+   //      return (size==INT_MAX)?0:size;
+   // }
+    int minSubArrayLen(int target, vector<int>& nums) { 
+        
+        int low=1,high=nums.size();
+        int res=0;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(isPossible(nums,mid,target)){
+                res=mid;
+                high=mid-1;
+            }else
+                low=mid+1;
         }
-        return (size==INT_MAX)?0:size;
-   }
+        return res==0?0:res;
+    }
+    
+    bool isPossible(vector<int> &nums, int size, int target){
+        int low=0,sum=0,count=0;
+        for(int i=0;i<nums.size();i++){
+            if(count>=size){
+                sum-=nums[low++];
+                count--;
+            }
+            sum+=nums[i];
+            count++;
+            if(sum>=target)
+                return true;
+        }
+        return false;
+    }
+    
 };
